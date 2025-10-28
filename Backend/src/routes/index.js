6ad75289ec589b71117e8714f231476593884f32
Guilherme Galdino <guigaldino.sp@gmail.com>
2025-10-27 22:07:56 -0300

    delete: Funções de endereço e parceiros removidos
    Arquivos desnecessários removidos pois não serão mais utilizados na nova versão do sistema

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const usuarioRoutes = require('./usuarioRoutes');
-const enderecoRoutes = require('./enderecoRoutes');
-const parceirosRoutes = require('./parceirosRoutes');
 
 router.use(usuarioRoutes);
-router.use(enderecoRoutes);
-router.use(parceirosRoutes);
 
 module.exports = router;
